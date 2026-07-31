be806b5e04e843e52150a160959ccfbedc91b29a
Ahmad Ali <103488484+ahmed-ali-waiz@users.noreply.github.com>
2026-07-31 16:07:13 +0000

    Number Smaller than index

---
+++ Day2/NumberSmaller.py
@@ -0,0 +1,11 @@
+arr = [1,2,3,4]
+
+result = []
+
+for i in range(len(arr)):
+    count = 0
+    for j in range(len(arr)):
+        if arr[j]<arr[i]:
+            count+=1
+    result.append(count)
+print(result)
